74bb6327f394e6d0312d55d5814c4b854f995cab
Saeed Shahsavan <s.shahsavan@gmail.com>
2025-09-08 10:59:15 +0200

    add hello world project

---
+++ hello-world/transport/cmd/vehiclectl/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// main is the program's entry point.
+// Every Go executable *must* have a main package and a main() function.
+// Later we'll break down functions, variables, and packages in detail.
+func main() {
+    fmt.Println("Hello, Go! The transportation system is starting...")
+}
---
+++ transport/go.mod
@@ -0,0 +1,3 @@
+module example.com/transport
+
+go 1.23.1
